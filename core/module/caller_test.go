da17b387f701700b2b9238fa5da69e92737270b4
Clivern <hello@clivern.com>
2020-07-22 23:30:30 +0200

    add test cases

--- core/module/caller_test.go
+++ core/module/caller_test.go
@@ -51,3 +51,93 @@ func TestCallerPostRequest(t *testing.T) {
 		pkg.Expect(t, true, strings.Contains(body, "POST"))
 	})
 }
+
+// TestCallerGetRequest test cases
+func TestCallerGetRequest(t *testing.T) {
+	t.Run("TestCallerGetRequest", func(t *testing.T) {
+		httpClient := NewHTTPClient()
+		caller := NewCaller(httpClient)
+		service := model.NewService("anything")
+
+		service.Endpoint[2].URI = "/anything"
+
+		fields01 := caller.GetFields(
+			fmt.Sprintf("%s - %s", service.Main.ID, service.Endpoint[2].ID),
+			service,
+		)
+
+		res, err := caller.Call(
+			fmt.Sprintf("%s - %s", service.Main.ID, service.Endpoint[2].ID),
+			service,
+			fields01,
+		)
+
+		pkg.Expect(t, nil, err)
+
+		body, err := httpClient.ToString(res)
+
+		pkg.Expect(t, nil, err)
+		pkg.Expect(t, 200, httpClient.GetStatusCode(res))
+		pkg.Expect(t, true, strings.Contains(body, "GET"))
+	})
+}
+
+// TestCallerPutRequest test cases
+func TestCallerPutRequest(t *testing.T) {
+	t.Run("TestCallerPutRequest", func(t *testing.T) {
+		httpClient := NewHTTPClient()
+		caller := NewCaller(httpClient)
+		service := model.NewService("anything")
+
+		service.Endpoint[3].URI = "/anything"
+
+		fields01 := caller.GetFields(
+			fmt.Sprintf("%s - %s", service.Main.ID, service.Endpoint[3].ID),
+			service,
+		)
+
+		res, err := caller.Call(
+			fmt.Sprintf("%s - %s", service.Main.ID, service.Endpoint[3].ID),
+			service,
+			fields01,
+		)
+
+		pkg.Expect(t, nil, err)
+
+		body, err := httpClient.ToString(res)
+
+		pkg.Expect(t, nil, err)
+		pkg.Expect(t, 200, httpClient.GetStatusCode(res))
+		pkg.Expect(t, true, strings.Contains(body, "PUT"))
+	})
+}
+
+// TestCallerDeleteRequest test cases
+func TestCallerDeleteRequest(t *testing.T) {
+	t.Run("TestCallerDeleteRequest", func(t *testing.T) {
+		httpClient := NewHTTPClient()
+		caller := NewCaller(httpClient)
+		service := model.NewService("anything")
+
+		service.Endpoint[4].URI = "/anything"
+
+		fields01 := caller.GetFields(
+			fmt.Sprintf("%s - %s", service.Main.ID, service.Endpoint[4].ID),
+			service,
+		)
+
+		res, err := caller.Call(
+			fmt.Sprintf("%s - %s", service.Main.ID, service.Endpoint[4].ID),
+			service,
+			fields01,
+		)
+
+		pkg.Expect(t, nil, err)
+
+		body, err := httpClient.ToString(res)
+
+		pkg.Expect(t, nil, err)
+		pkg.Expect(t, 200, httpClient.GetStatusCode(res))
+		pkg.Expect(t, true, strings.Contains(body, "DELETE"))
+	})
+}
